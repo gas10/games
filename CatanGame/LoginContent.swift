591f64ca2ccb9f02517fa7ccdbd2f2d2fe38a84d
Amar <amarspggawade@gmail.com>
2020-08-09 03:23:30 -0400

    Added login page

---
+++ CatanGame/LoginContent.swift
@@ -0,0 +1,21 @@
+//
+//  LoginContent.swift
+//  CatanGame
+//
+//  Created by Amar Gawade on 8/9/20.
+//  Copyright © 2020 Amar Gawade. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginContent_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginContent()
+    }
+}
--- CatanGame/SceneDelegate.swift
+++ CatanGame/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // If using a storyboard, the `window` property will automatically be initialized "Amarand attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Get the managed object context from the shared persistent container.
@@ -24,7 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = ContentView().environment(\.managedObjectContext, context)
+        let contentView = ContentView(username: "Amar", password: "Amar").environment(\.managedObjectContext, context)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
